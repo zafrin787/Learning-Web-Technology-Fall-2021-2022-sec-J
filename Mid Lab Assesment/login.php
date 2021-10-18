ff7af575f9ec18ff00c894c8539b7c21a487e103
zafrin787 <78695279+zafrin787@users.noreply.github.com>
2021-10-18 23:49:00 +0600

    Mid Assesment

---
+++ Mid Lab Assesment/login.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        <h1>Login</h1>
+        <form action="handleLogin.php" method="post">
+            <label for="id">User Id</label>
+            <input type="text" name="id">
+            <label for="password">Pasword</label>
+            <input type="text" name="password">
+            <input type="submit" name="submit" value="Submit">
+        </form>
+        <a href="registration.php">Registration</a>
+    </body>
+</html>
